bb0b91660644c994266a797227aeebbae2044bcd
shellyrgarner <sgrenea@gmail.com>
2018-08-06 20:50:32 -0400

    all is working

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -2,14 +2,14 @@ var mysql = require("mysql");
 var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
-    host:"localhost",
+    host: "localhost",
     port: 3306,
     user: "root",
     password: "root",
     database: "bamazondb"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
 
     startApp();
@@ -17,48 +17,126 @@ connection.connect(function(err) {
 
 function startApp() {
     connection.query(
-        "SELECT * FROM products", function(err, res) {
+        "SELECT * FROM products", function (err, res) {
             // console.log(answer);
             if (err) throw err;
-            for(var i=0; i<res.length;i++)
-            {
-                console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);      
+            for (var i = 0; i < res.length; i++) {
+                console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name + " | " + res[i].price + " | " + res[i].stock_quantity);
             }
 
-    inquirer
-    .prompt([
-     {
-        name: "itemID",
-        type: "input",
-        message: "What is the ID of the product you would like to buy?"
-    },
-    {
-        name: "units",
-        type: "input",
-        message: "How many units?"
-    }
-    ])
-    .then(function(answer) {
-        var chosenID;
-        for (var i=0; i<res.length;i++) {
-            console.log(answer);
-            if (res[i].item_id == answer.item_id) {
-                chosenID = res[i];
+            inquirer
+                .prompt([
+                    {
+                        name: "itemID",
+                        type: "input",
+                        message: "What is the ID of the product you would like to buy?"
+                    },
+                    {
+                        name: "units",
+                        type: "input",
+                        message: "How many units?"
+                    }
+                ])
+                .then(function (answer) {
+                    var chosenID;
+                    var quantityRemaining = 0;
 
-                // console.log(chosenID);
-                // console.log(answer.item);
-                // console.log("resid: " + res[i]);
-            }
-            // console.log("resid: " + res[i]);
-            // console.log("chosenid: " + chosenID);
-            // console.log("answer_item: " + answer.item_id);
+                    for (var i = 0; i < res.length; i++) {
+                     
+                        if (res[i].item_id == answer.itemID && res[i].stock_quantity > 0) {
+                            chosenID = res[i].item_id;
+                            quantityRemaining += res[i].stock_quantity;
+
+                            console.log("It's your lucky day! We have " + res[i].stock_quantity + " in stock.  Enjoy your purchase.")
+                            quantityRemaining -= answer.units;
+
+                            connection.query(
+                                "UPDATE products SET ? WHERE ?",
+                                [
+                                    {
+                                        stock_quantity: quantityRemaining
+                                    },
+                                    {
+                                        item_id: answer.itemID
+                                    }
+                                ],
+                                function (error) {
+                                    if (error) throw err;
+                                    // console.log("Stock updated successfully!");
+                                    //  console.log("------------------------------------------");
+
+                                    // restart();
+                                }
+                            );
+                            console.log("Stock updated successfully!");
+                            console.log("Quantity remaining for " + res[i].product_name + ": " + quantityRemaining);
+                            
+                                     console.log("------------------------------------------");
+
+                                    restart();
+                        }
+                        else if (res[i].item_id == answer.itemID && res[i].stock_quantity == 0) {
+                            console.log("Insufficient quantity!");
+                            console.log("Please choose another item!");
+                            console.log("------------------------------------------");
+
+                            restart();
+                        }
+                    }
+                });
         }
-        // console.log("resid: " + res[i]);
-        // console.log("chosenid: " + chosenID);
-        // console.log("answer_item: " + answer.item_id);
+    )
+};
 
-        // if(chosenID)
-        // console.log(res);
-    });
+// function updateInventory(answer) {
+//     connection.query(
+//         "SELECT * FROM products", function (err, res) {
+//             // console.log(answer);
+//             if (err) throw err;
+//             var chosenID;
+//             var quantityRemaining = 0;
+
+//             for (var i = 0; i < res.length; i++) {
+
+//                 if (res[i].item_id == answer.itemID && res[i].stock_quantity > 0) {
+//                     chosenID = res[i].item_id;
+//                     quantityRemaining += res[i].stock_quantity;
+
+//                     console.log("It's your lucky day! We have " + res[i].stock_quantity + " in stock.  Enjoy your purchase.")
+//                     quantityRemaining -= answer.units;
+
+//                     connection.query(
+//                         "UPDATE products SET ? WHERE ?",
+//                         [
+//                             {
+//                                 stock_quantity: quantityRemaining
+//                             },
+//                             {
+//                                 item_id: answer.itemID
+//                             }
+//                         ],
+//                         function (error) {
+//                             if (error) throw err;
+//                             console.log("Stock for " + res[i].product_name + " updated successfully!");
+//                         }
+//                     );
+//                     // console.log("Quantity remaining for " + res[i].product_name + ": " + quantityRemaining);
+//                 }
+//                 else if (res[i].item_id == answer.itemID && res[i].stock_quantity == 0) {
+//                     console.log("Insufficient quantity!");
+//                     console.log("Please choose another item!");
+//                     startApp();
+//                 }
+
+//             }
+//         }
+//     )
+// };
+
+
+function restart() {
+
+
+    startApp();
+
 }
-)}
